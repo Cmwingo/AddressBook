dc5fc4be2368bc164ee3898b1cc39a9dab46c287
Wingo <cmwingo@gmail.com>
2016-11-18 08:35:21 -0800

    Set up initial files and created the Contact class

---
+++ Objects/Contact.cs
@@ -0,0 +1,73 @@
+using System.Collections.Generic;
+
+namespace AddressBook.Objects
+{
+  public class Contact
+  {
+    private string _firstName;
+    private string _lastName;
+    private string _phone;
+    private string _address;
+    private int _id;
+    private static List<Contact> _instances = new List<Contact>();
+
+    public Contact(string firstName, string lastName, string phone, string address)
+    {
+      _firstName = firstName;
+      _lastName = lastName;
+      _phone = phone;
+      _address = address;
+      _instances.Add(this);
+      _id = _instances.Count;
+    }
+
+    public string GetFirstName()
+    {
+      return _firstName;
+    }
+    public void SetFirstName(string newFirstName)
+    {
+      _firstName = newFirstName;
+    }
+    public string GetLastName()
+    {
+      return _lastName;
+    }
+    public void SetLastName(string newLastName)
+    {
+      _lastName = newLastName;
+    }
+    public string GetPhone()
+    {
+      return _phone;
+    }
+    public void SetPhone(string newPhone)
+    {
+      _phone = newPhone;
+    }
+    public string GetAddress()
+    {
+      return _address;
+    }
+    public void SetAddress(string newAddress)
+    {
+      _address = newAddress;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public static List<Contact> GetAll()
+    {
+      return _instances;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+    public static Contact Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+  }
+}
